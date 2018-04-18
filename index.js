ae086a1f0c98166b9b32531605252dea5d69ce58
ThisTNTSquid <tntsquid@corecrafted.net>
2018-04-19 00:11:20 +0900

    Setup logger

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
+// load libs
 const path = require("path");
+const logger = require('./utils/logger')
+// setup discord,js
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
+// load config
+let config
+try{
+  config = require('./config.json')
+} catch(e){
+  console.error()
+  console.log("=========================")
+  console.log('TIPS: Have you rename \'config-example.json\' to \'config.json\' yet?')
+  process.exit()
+}
+
 // Bot initialization handler
-bot.on("ready", () => {
+bot.on("ready", async () => {
   console.log("Bot Started");
-  console.log(`Invite the bot to your server with\n \
-  =======================================\n \
-  ${bot.generateInvite([
-    "ADD_RECTIONS",
+  console.log('Invite the bot to your server with\n')
+  let link = await bot.generateInvite([
+    "ADD_REACTIONS",
     "VIEW_CHANNEL",
     "SEND_MESSAGES",
     "READ_MESSAGE_HISTORY",
@@ -17,9 +30,8 @@ bot.on("ready", () => {
     "USE_EXTERNAL_EMOJIS",
     "MENTION_EVERYONE",
     "ATTACH_FILES"
-  ])}\
-  ===========================================
-  `);
+  ])
+  console.log(link)
 });
 
-bot.login('')
+bot.login(config.token)
---
+++ utils/logger.js
@@ -0,0 +1,5 @@
+module.export = {
+  info(msg) {
+    console.log(`[INFO] ${msg}`);
+  }
+};
