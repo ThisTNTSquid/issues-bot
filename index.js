bc6966bf74fea773babed235e538b01d39312280
ThisTNTSquid <tntsquid@corecrafted.net>
2018-04-19 14:35:29 +0900

    disable logging for now

--- index.js
+++ index.js
@@ -1,38 +1,47 @@
 // load libs
 const path = require("path");
-const Logger = require('./utils/logger')
-const logger = new Logger()
+
 // setup discord,js
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 // load config
-let config
-try{
-  config = require('./config.json')
-} catch(e){
-  console.error(e)
-  console.log("=========================")
-  console.log('TIPS: Have you rename \'config-example.json\' to \'config.json\' yet?')
-  process.exit()
+let config;
+try {
+  config = require("./config.json");
+} catch (e) {
+  console.error(e);
+  console.log("=========================");
+  console.log(
+    "TIPS: Have you rename 'config-example.json' to 'config.json' yet?"
+  );
+  process.exit();
 }
 
 // Bot initialization handler
 bot.on("ready", async () => {
   logger.info("Bot Started");
   logger.info("Invite the bot to your server with\n");
-  let link = await bot.generateInvite([
-    "ADD_REACTIONS",
-    "VIEW_CHANNEL",
-    "SEND_MESSAGES",
-    "READ_MESSAGE_HISTORY",
-    "MANAGE_MESSAGES",
-    "EMBED_LINKS",
-    "USE_EXTERNAL_EMOJIS",
-    "MENTION_EVERYONE",
-    "ATTACH_FILES"
-  ])
-  logger.info(link);
+  try {
+    let link = await bot.generateInvite([
+      "ADD_REACTIONS",
+      "VIEW_CHANNEL",
+      "SEND_MESSAGES",
+      "READ_MESSAGE_HISTORY",
+      "MANAGE_MESSAGES",
+      "EMBED_LINKS",
+      "USE_EXTERNAL_EMOJIS",
+      "MENTION_EVERYONE",
+      "ATTACH_FILES"
+    ]);
+    logger.info(link);
+  } catch (e) {
+    console.error(e);
+    
+  }
 });
 
-bot.login(config.token)
+// handle problems
+bot.on("error", console.error());
+
+bot.login(config.token);
